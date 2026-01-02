5817cb1b3bfe6245e4cbb864d89a32ee41029d01
bernikr <git@berni.kr>
2026-01-02 18:33:12 +0100

    mount filesystems

--- create_vm.sh
+++ create_vm.sh
@@ -1,24 +1,25 @@
 #!/usr/bin/env sh
 
 export VM_ID=123
+export DATA_SIZE=5
+export CACHE_SIZE=5
 
 set -e
 
 # compile config
 podman run --rm -i --volume ${PWD}:/pwd quay.io/coreos/butane:latest -d /pwd < butane.yaml > ignition.json
 
-
-# shutdown and remove previous vm
-qm shutdown $VM_ID
-qm destroy $VM_ID
-
 # download the flatcar image if it does not exist
 if ! test -f "flatcar_production_proxmoxve_image.img"; then
   wget https://stable.release.flatcar-linux.net/amd64-usr/current/flatcar_production_proxmoxve_image.img
 fi
 
+# shutdown and remove previous vm
+qm shutdown $VM_ID
+qm destroy $VM_ID
+
 # create the vm and import the image to it's disk
-qm create $VM_ID --cores 2 --memory 4096 --net0 "virtio,bridge=vmbr0" --ipconfig0 "ip=dhcp"
+qm create $VM_ID --name "flatcar" --cores 2 --memory 4096 --net0 "virtio,bridge=vmbr0" --ipconfig0 "ip=dhcp"
 qm disk import $VM_ID flatcar_production_proxmoxve_image.img local-lvm
 
 # tell the vm to boot from the imported image
@@ -32,6 +33,10 @@ qm set $VM_ID --ide2 local-lvm:cloudinit
 # copy over the ignition config and set the vm to it
 cp ./ignition.json /var/lib/vz/snippets/ignition
 qm set $VM_ID --cicustom "user=local:snippets/ignition"
+
+# create the data and cache disks
+qm set $VM_ID --scsi1 local-lvm:$DATA_SIZE,ssd=1,discard=on,serial=data
+qm set $VM_ID --scsi2 local-lvm:$CACHE_SIZE,ssd=1,discard=on,serial=cache
 
 # boot the VM
 qm start $VM_ID
