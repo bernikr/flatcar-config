8cd7a2b8e5abff26e8ba6a77cbc2841951ec43eb
bernikr <git@berni.kr>
2026-04-21 15:21:48 +0200

    remove adsb dongle

--- create_vm.sh
+++ create_vm.sh
@@ -64,9 +64,6 @@ qm set $VM_ID --scsihw virtio-scsi-single
 # set the gpu passthrough
 qm set $VM_ID --hostpci0 0000:00:02.0
 
-# pass throgh rtl-sdr
-qm set $VM_ID -usb0 host=0bda:2838
-
 # set the instruction set to host for best performance (prevents migration to other hosts)
 qm set $VM_ID --cpu host
 
